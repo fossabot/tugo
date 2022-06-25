a4987d3d72d66e7b8580013a02e84c5b40f21404
Marco <marco@marcozac.com>
2022-06-25 21:43:57 +0200

    feat: add `IgnoreDir` functional option to `TempDir`

--- tempdir.go
+++ tempdir.go
@@ -2,6 +2,7 @@ package tugo
 
 import (
 	"os"
+	"path/filepath"
 )
 
 type tempDir struct {
@@ -27,17 +28,39 @@ type ITempDir interface {
 	Remove() error
 }
 
+// TempDirOpt is a TempDir functional option. Its argument
+// is the temporary directory path.
+type TempDirOpt func(string) error
+
 // TempDir is a wrapper of os MkdirTemp method from which it inherits
 // the same arguments: https://pkg.go.dev/os?utm_source=gopls#MkdirTemp.
 // It returns ITempDir, an interface withe the directory path getter and
 // the method to remove it. It is caller's reponsability to call Remove
 // when it is no longer needed.
-func TempDir(dir string, pattern string) (ITempDir, error) {
+func TempDir(dir string, pattern string, opts ...TempDirOpt) (ITempDir, error) {
 	var err error
 	d := new(tempDir)
 	d.path, err = os.MkdirTemp(dir, pattern)
 	if err != nil {
 		return nil, err
 	}
+
+	for _, opt := range opts {
+		err = opt(d.path)
+		if err != nil {
+			_ = d.Remove()
+			return nil, err
+		}
+	}
+
 	return d, nil
+}
+
+// IgnoreDir is an option for TempDir that writes a .gitignore file
+// with content "*" to ignore all files in the temporary directory.
+func IgnoreDir() TempDirOpt {
+	return func(d string) error {
+		data := []byte("*")
+		return os.WriteFile(filepath.Join(d, ".gitignore"), data, 0766)
+	}
 }
--- tempdir_test.go
+++ tempdir_test.go
@@ -2,6 +2,7 @@ package tugo
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -9,6 +10,27 @@ func TestTempDir(t *testing.T) {
 	d, err := TempDir(".", "testing")
 	if err != nil {
 		t.Fatal("failed to create temporary directory:", err)
+	}
+
+	if err := d.Remove(); err != nil {
+		t.Fatal("failed to remove temporary directory:", err)
+	}
+
+	if _, err := os.Open(d.Path()); err == nil {
+		t.Fatal("temporary directory has not been removed")
+	}
+}
+
+func TestTempDirIgnore(t *testing.T) {
+	d, err := TempDir(".", "testing-ignore", IgnoreDir())
+	if err != nil {
+		t.Fatal("failed to create temporary directory:", err)
+	}
+
+	if f, err := os.Open(filepath.Join(d.Path(), ".gitignore")); err != nil {
+		t.Error(".gitignore file not found in the temporary directory")
+	} else {
+		_ = f.Close()
 	}
 
 	if err := d.Remove(); err != nil {
